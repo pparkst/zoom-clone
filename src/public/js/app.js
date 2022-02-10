46dc91fe700e521921518f1cbeaec53696cac476
pparkst <pparkst@naver.com>
2022-02-10 20:55:01 +0900

    add: disconnect event, send message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,6 +15,17 @@ function addMessage(message){
     ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {    
+    event.preventDefault();
+    const input = room.querySelector("input");
+    const message = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${message}`);
+    });
+ 
+    input.value = "";
+}
+
 function handleRoomSubmit(event){
     event.preventDefault();
     const input = document.querySelector("input");
@@ -24,6 +35,9 @@ function handleRoomSubmit(event){
         h3.innerText = `Room ${roomName}`
         welcome.hidden = true;
         room.hidden = false;
+
+        const form = room.querySelector("form");
+        form.addEventListener("submit", handleMessageSubmit)
     });
     roomName = input.value;
     input.value = "";
@@ -34,4 +48,12 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("welcome", () => {
     console.log("welcome event!")
     addMessage("someone joined!");
+})
+
+socket.on("bye", () => {
+    addMessage("someone left ㅠㅠ");
+})
+
+socket.on("new_message", (message) => {
+    addMessage(message);
 })
--- src/server.js
+++ src/server.js
@@ -25,6 +25,17 @@ io.on("connection", socket => {
         done();
         socket.to(roomName).emit("welcome");
     });
+
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => {
+            socket.to(room).emit("bye");
+        });
+    });
+
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
+    })
 });
 
 /* 
