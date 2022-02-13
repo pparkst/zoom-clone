e8b5751f2a3ef3b0d23ff149851de50fde63f6a4
pparkst <pparkst@naver.com>
2022-02-13 21:23:31 +0900

    init: peerToConnection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,9 +5,14 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
+const call = document.getElementById("call");
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName = "";
+let myPeerConnection;
 
 async function getCameras() {
     try{
@@ -60,8 +65,6 @@ async function getMedia(deviceId) {
     }
 }
 
-getMedia();
-
 function handleMuteBtnClick() {
     console.log(myStream.getAudioTracks());
     myStream
@@ -96,8 +99,54 @@ async function handleCameraChange() {
 
 muteBtn.addEventListener("click", handleMuteBtnClick);
 cameraBtn.addEventListener("click", handleCameraBtnClick);
-
 camerasSelect.addEventListener("input", handleCameraChange);
+
+
+// Welcome Form (choose a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function startMedia() {
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
+function handleWelcomeSubmit(event) {
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    socket.emit("join_room", input.value, startMedia);
+    roomName = input.value;
+    input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket COde
+
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+
+    socket.emit("offer", offer, roomName);
+    console.log("send offer");
+});
+
+socket.on("offer", (offer) => {
+    console.log(offer);
+});
+
+
+//RTC COde
+
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection();
+    myStream
+        .getTracks()
+        .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
 
 
 
--- src/server.js
+++ src/server.js
@@ -20,6 +20,18 @@ const io = SocketIO(httpServer);
 httpServer.listen(3000, handleListen);
 
 
+io.on("connection", socket => {
+    socket.on("join_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+})
+
+
 /* const io = new Server(httpServer, {
     cors: {
         origin: ["https://admin.socket.io"],
